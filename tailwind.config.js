63b736dd39f6fe18e34675940928baf34cc9ff99
ycarpenter <yurii.carpenter@gmail.com>
2023-07-18 12:32:40 +0300

    feat: setup color palete

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        default: '#3A3D60',
+        primary: '#40CCFF',
+        secondary: '#FFCF00',
+      },
+      boxShadow: {
+        'gradient': '0 0 0 3px #FFCF00, 0 0 0 5px #40CCFF)',
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-linear': 'linear-gradient(89deg, #40CCFF 0%, #FFCF00 100%)',
       },
     },
   },
