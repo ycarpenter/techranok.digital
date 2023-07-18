36bfacc809f85574add53d9a267bd15c20f24d9a
ycarpenter <yurii.carpenter@gmail.com>
2023-07-18 12:39:04 +0300

    feat: implement header, nav

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,18 @@
+import Header from './(components)/Header/Header'
+
 import Image from 'next/image'
 import { useTranslations } from 'next-intl'
+import Wrapper from './(components)/Wrapper/Wrapper'
 
 export default function Home() {
-  return <main></main>
+  return (
+    <>
+      <Header />
+      <main>
+        <Wrapper>
+          <section></section>
+        </Wrapper>
+      </main>
+    </>
+  )
 }
